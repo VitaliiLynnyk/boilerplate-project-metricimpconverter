083c3c3b5e6a325934ab69c34aea548fa0e6f0b4
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-23 20:30:53 +0100

    format converted number

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -10,16 +10,21 @@ function patternExecute(value, pattern) {
   return pattern.exec(value)[0].replace(/\s/g, '');
 }
 
+function toFormatNumber(numb) {
+  return Math.floor(numb * 100) / 100;
+}
+
 function ConvertHandler() {
   
   this.getNum = input => {
     const units = [ 'gal', 'l', 'mi', 'km', 'lbs', 'kg' ];
     const pattern = /\d?[^a-z]*/;
     const patternExec = patternExecute(input, pattern);
+    const numb = Number(patternExec);
     
     if (units.includes(input)) return 1;
     
-    return Number(patternExec) ? patternExec : 'invalid number';
+    return numb ? numb : 'invalid number';
   };
   
   this.getUnit = input => {
@@ -74,26 +79,35 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    let convertedNumb;
     
     switch (initUnit.toLowerCase()) {
       case 'mi' :
-        return initNum * miToKm;
+        convertedNumb = initNum * miToKm;
+        break;
       case 'km' :
-        return initNum / miToKm;
+        convertedNumb = initNum / miToKm;
+        break;
       case 'gal':
-        return initNum * galToL;
+        convertedNumb = initNum * galToL;
+        break;
       case 'l'  :
-        return initNum / galToL;
+        convertedNumb = initNum / galToL;
+        break;
       case 'lbs':
-        return initNum * lbsToKg;
+        convertedNumb = initNum * lbsToKg;
+        break;
       case 'kg' :
-        return initNum / lbsToKg;
+        convertedNumb = initNum / lbsToKg;
+        break;
       default   :
         return 'ERROR';
     }
+    
+    return toFormatNumber(convertedNumb);
   };
   
-  this.getString = (initNum, initUnit, returnNum, returnUnit) => `initNum ${this.spellOutUnit(initUnit)} converts to ${returnNum.toFixed(5)} ${this.spellOutUnit(returnUnit)}`;
+  this.getString = (initNum, initUnit, returnNum, returnUnit) => `${initNum} ${this.spellOutUnit(initUnit)} converts to ${toFormatNumber(returnNum)} ${this.spellOutUnit(returnUnit)}`;
 }
 
 module.exports = ConvertHandler;
