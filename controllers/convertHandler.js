88ed93452a8320c5b53d53f70a33e5cbf3f8155c
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-23 18:36:04 +0100

    add getNum logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -8,31 +8,32 @@
 
 function ConvertHandler() {
   
-  this.getNum = function(input) {
+  this.getNum = input => {
+    const pattern = /\d?[^a-z]*/;
+    if (input.toLowerCase() === 'gal' | 'lbs' | 'mi' | 'l' | 'kg' | 'km') return 1;
+    const numberPart = pattern.exec(input)[0].replace(/\s/g, '');
+    return Number(numberPart) ? numberPart : 'invalid number';
+  };
+  
+  this.getUnit = function (input) {
     var result;
     
     return result;
   };
   
-  this.getUnit = function(input) {
+  this.getReturnUnit = function (initUnit) {
     var result;
     
     return result;
   };
   
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
-  };
-
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     var result;
     
     return result;
   };
   
-  this.convert = function(initNum, initUnit) {
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
@@ -41,7 +42,7 @@ function ConvertHandler() {
     return result;
   };
   
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     var result;
     
     return result;
